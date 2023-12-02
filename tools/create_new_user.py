c65666ca144380f541c131a61a366bfa620ed45a
Michal Kuszewski <michal.kuszewski@pl.abb.com>
2023-12-02 20:33:11 +0100

    initial

--- tools/create_new_user.py
+++ tools/create_new_user.py
@@ -5,10 +5,4 @@ from streamlit_tools import execute_query
 def new_user(login, UserName, Password):
     execute_query(f"call p_add_user('{login}','{UserName}', '{Password}')", return_type="df")
 
-hashed_passwords  = stauth.Hasher(['123']).generate()
-
-
-        
-
-# new_user('Adamus', 'Adamus Bóg Krwi',  hashed_passwords[0])
-# new_user('Lusi', 'Lusi The Queen', hashed_passwords[0])
+hashed_passwords  = stauth.Hasher(['xxx']).generate()
